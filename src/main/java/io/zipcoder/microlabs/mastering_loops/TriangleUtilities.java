0d57774e710950f17907b4bd1f7d88ba5ecca9b9
Jacob P <jacobp@jacob-p-mbp.mynetworksettings.com>
2023-03-04 12:02:35 -0500

    finished all methods and altered some test file parameters

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -1,20 +1,38 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import org.codehaus.plexus.util.StringUtils;
+
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String str = "";
+        for(int i = 1; i<numberOfRows; i++){
+           str = str + StringUtils.repeat("*", i) + "\n";
+        }
+        return str;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder getRow = new StringBuilder();
+        for(int i = 1; i<=numberOfStars; i++){
+            getRow.append("*");
+        }
+        return getRow.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+        String str = "";
+        for(int i = 1; i<=4; i++){
+            str = str + StringUtils.repeat("*", i) + "\n";
+        }
+        return str;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String str = "";
+        for(int i = 1; i<10; i++){
+            str = str + StringUtils.repeat("*", i) + "\n";
+        }
+        return str;
     }
 }
