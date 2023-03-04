f74156d5af79cec0f12da999245b3bc59e47c558
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-04 15:12:45 -0500

    Finished TableUtilities.java and tested

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,40 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
-    public static String getSmallMultiplicationTable() {
-        return null;
+    public static String getSmallMultiplicationTable(int width) {
+        String str = "";
+        for(int i = 1; i <= width; i++){
+            for(int k = 1; k <= width; k++){
+                str = str + String.format("%5s", i * k + " |");
+            }
+            str = str + "\n";
+        }
+
+        return str;
     }
 
-    public static String getLargeMultiplicationTable() {
-        return null;
+    public static String getLargeMultiplicationTable(int width) {
+        String str = "";
+        for(int i = 1; i <= width; i++){
+            for(int k = 1; k <= width; k++){
+                str = str + String.format("%5s", i * k + " |");
+            }
+            str = str + "\n";
+        }
+
+
+        return str;
     }
 
-    public static String getMultiplicationTable(int tableSize) {
-        return null;
+    public static String getMultiplicationTable(int width) {
+        String str = "";
+        for(int i = 1; i <= width; i++){
+            for(int k = 1; k <= width; k++){
+                str = str + String.format("%5s", i * k + " |");
+            }
+            str = str + "\n";
+        }
+
+        return str;
     }
 }
